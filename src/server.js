ac08e2321ad7577e1b1c2ccb798e44ccb40694e2
brumattih <hrpreis@gmail.com>
2020-06-08 22:37:08 -0300

    Including CORS

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const routes = require('./routes')
-
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    app.use(cors())
+    next()
+})
+
 app.use(routes)
 
 
