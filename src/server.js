f90b34c1b041f186ee743fed89b8e7ab0fd27417
brumattih <hrpreis@gmail.com>
2020-06-09 19:37:15 -0300

    Fixing the cors config

--- src/server.js
+++ src/server.js
@@ -6,13 +6,14 @@ const app = express()
 
 app.use(express.json())
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", 'GET, PUST, POST, DELETE')
     res.header("Access-Control-Allow-Headers", 'CONTENT TYPE')
     app.use(cors())
     next()
-})
+})*/
+app.use(cors())
 
 app.use(routes)
 
