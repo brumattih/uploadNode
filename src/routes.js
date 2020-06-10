ad31170b96e43fb005e794e9e2cdc8b5f475c9cb
brumattih <hrpreis@gmail.com>
2020-06-10 20:15:50 -0300

    adding getScore

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const UserController = require('./controllers/UserController')
 
 routes.get('/players', UserController.index)
 routes.get('/ranking', UserController.getRanking)
+routes.get('/score', UserController.getScore)
 routes.post('/questions', UserController.questions)
 routes.post('/players', UserController.createPlayer)
 routes.post('/logon', UserController.logon)
